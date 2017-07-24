85d824e90d22ff57a1264030803c2562d2a181ca
elpinal <elpinal@users.noreply.github.com>
2017-07-25 07:04:14 +0900

    Update test

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,5 +4,17 @@ import Lib
 main :: IO ()
 main = hspec $ do
   describe "translate" $ do
-    it "translates existing alphabet mappings to commands" $ do
+    it "translates an existing alphabet mapping to a command" $ do
       translate [Alphabet 'j'] `shouldBe` [Accept $ Command "moveDown"]
+
+    it "translates multiple alphabet mappings to commands" $ do
+      translate [Alphabet 'j', Alphabet 'k'] `shouldBe` [Accept $ Command "moveDown", Accept $ Command "moveUp"]
+
+    it "does nothing with no existing alphabet mappings" $ do
+      translate [Alphabet 'h', Alphabet 'Q', Alphabet 'k'] `shouldBe` [Accept $ Command "moveToLeft", Accept $ Command "moveUp"]
+
+    it "does nothing when given empty list" $ do
+      translate [] `shouldBe` []
+
+    it "translates sequential alphabets to a mapped command" $ do
+      translate [Alphabet 'h', Alphabet 'i', Alphabet '!'] `shouldBe` [Accept $ Command "hi!"]
