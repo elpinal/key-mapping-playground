1b0ed3d8659d99c993a952cb86259d1d13c1d55e
elpinal <elpinal@users.noreply.github.com>
2017-07-25 07:51:47 +0900

    Enable to switch modes

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,6 +21,12 @@ data Mapping = Mapping [Alphabet] [Alphabet]
 
 type Mappings = Map.Map [Alphabet] [Alphabet]
 
+modeMap :: Map.Map Mode (Map.Map [Alphabet] Command)
+modeMap = Map.fromList
+  [ (Normal, normalMappings)
+  , (Insert, insertMappings)
+  ]
+
 -- Note: Don't use empty list for keys.
 normalMappings :: Map.Map [Alphabet] Command
 normalMappings = Map.fromList
@@ -42,9 +48,9 @@ data Result = Accept Command
             | Pending
             deriving (Show, Eq)
 
-translate :: [Alphabet] -> [Result]
-translate [] = []
-translate xs = translateN normalMappings normalMappings xs 0
+translate :: Map.Map [Alphabet] Command -> [Alphabet] -> [Result]
+translate _ [] = []
+translate m xs = translateN m m xs 0
 
 translateN :: Map.Map [Alphabet] Command -> Map.Map [Alphabet] Command -> [Alphabet] -> Int -> [Result]
 translateN original ps xs n =
@@ -57,16 +63,21 @@ translateN original ps xs n =
   in
     case length ps' of
       0 -> backtrack c0 xs n original
-      1 | isJust c -> result c : translate (tail xs')
+      1 | isJust c -> result c : translate (selectMappings original $ Accept $ fromJust c) (tail xs')
       _ -> if length xs == n+1 then [result c] else translateN original ps' xs (n+1)
 
 backtrack :: Maybe Result -> [Alphabet] -> Int -> Map.Map [Alphabet] Command -> [Result]
-backtrack c0 xs n original = if isJust c0 then fromJust c0 : translate (drop n xs) else
+backtrack c0 xs n original = if isJust c0 then fromJust c0 : translate (selectMappings original (fromJust c0)) (drop n xs) else
   let
     c1 = Accept <$> Map.lookup (take (n-1) xs) original
   in
-    if n == 0 then translate (tail xs) else backtrack c1 xs (n-1) original
+    if n == 0 then translate original (tail xs) else backtrack c1 xs (n-1) original
 
 result :: Maybe Command -> Result
 result (Just x) = Accept x
 result Nothing = Pending
+
+selectMappings :: Map.Map [Alphabet] Command -> Result -> Map.Map [Alphabet] Command
+selectMappings original (Accept (Name _)) = original
+selectMappings original (Accept (Enter m)) = Map.findWithDefault (error $ "no such mode in mode map: " ++ show m) m modeMap
+selectMappings _ Pending = error "unexpected Pending"
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,16 +5,16 @@ main :: IO ()
 main = hspec $ do
   describe "translate" $ do
     it "translates an existing alphabet mapping to a command" $ do
-      translate [Alphabet 'j'] `shouldBe` [Accept $ Name "moveDown"]
+      translate normalMappings [Alphabet 'j'] `shouldBe` [Accept $ Name "moveDown"]
 
     it "translates multiple alphabet mappings to commands" $ do
-      translate [Alphabet 'j', Alphabet 'k'] `shouldBe` [Accept $ Name "moveDown", Accept $ Name "moveUp"]
+      translate normalMappings [Alphabet 'j', Alphabet 'k'] `shouldBe` [Accept $ Name "moveDown", Accept $ Name "moveUp"]
 
     it "does nothing with no existing alphabet mappings" $ do
-      translate [Alphabet 'h', Alphabet 'Q', Alphabet 'k'] `shouldBe` [Accept $ Name "moveToLeft", Accept $ Name "moveUp"]
+      translate normalMappings [Alphabet 'h', Alphabet 'Q', Alphabet 'k'] `shouldBe` [Accept $ Name "moveToLeft", Accept $ Name "moveUp"]
 
     it "does nothing when given empty list" $ do
-      translate [] `shouldBe` []
+      translate normalMappings [] `shouldBe` []
 
     it "translates sequential alphabets to a mapped command" $ do
-      translate [Alphabet 'h', Alphabet 'i', Alphabet '!'] `shouldBe` [Accept $ Name "hi!"]
+      translate normalMappings [Alphabet 'h', Alphabet 'i', Alphabet '!'] `shouldBe` [Accept $ Name "hi!"]
