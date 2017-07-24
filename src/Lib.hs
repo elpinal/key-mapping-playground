2cd5b7afcb29423e5adee376f4cda8a7e37aecf9
elpinal <elpinal@users.noreply.github.com>
2017-07-24 18:34:26 +0900

    Implement basics

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,48 @@ module Lib
     ( someFunc
     ) where
 
+import qualified Data.Map.Lazy as Map
+import Data.Maybe
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+newtype Alphabet = Alphabet Char deriving (Eq, Ord)
+
+newtype Command = Command String deriving (Show)
+
+data Mapping = Mapping [Alphabet] [Alphabet]
+
+type Mappings = Map.Map [Alphabet] [Alphabet]
+
+commandMappings :: Map.Map [Alphabet] Command
+commandMappings = Map.fromList
+                  [ ([Alphabet 'h'], Command "moveToLeft")
+                  , ([Alphabet 'j'], Command "moveDown")
+                  , ([Alphabet 'k'], Command "moveUp")
+                  , ([Alphabet 'l'], Command "moveToRight")
+                  , ([Alphabet 'h', Alphabet '!'], undefined)]
+
+data Result = Accept Command
+            | Pending
+            deriving (Show)
+
+translate :: [Alphabet] -> [Result]
+translate [] = []
+translate (x:xs) =
+  let
+    ps = Map.filterWithKey (\(k:ks) _ -> x == k) commandMappings
+    ps' = Map.filterWithKey (\(k:k1:ks) _ -> head xs == k1) ps
+  in
+    case length ps of
+      0 -> translate xs
+      1 -> (Accept $ snd (Map.findMin ps)) : translate xs
+      otherwise -> if null xs then [result (Map.lookup [x] ps)] else 
+                     case length ps' of
+                       0 -> maybeToList (Accept <$> Map.lookup [x] ps') ++ translate xs
+                       1 -> (Accept $ snd (Map.findMin ps)) : translate (tail xs)
+                       otherwise -> undefined
+
+result :: Maybe Command -> Result
+result (Just x) = Accept x
+result Nothing = Pending
