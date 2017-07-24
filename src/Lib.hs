eca13571e0c611eefbc94eef355d5ae8c4547d27
elpinal <elpinal@users.noreply.github.com>
2017-07-24 19:34:38 +0900

    Enable to parse commands whose length is 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,9 +9,9 @@ import Safe
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-newtype Alphabet = Alphabet Char deriving (Eq, Ord)
+newtype Alphabet = Alphabet Char deriving (Show, Eq, Ord)
 
-newtype Command = Command String deriving (Show)
+newtype Command = Command String deriving (Show, Eq)
 
 data Mapping = Mapping [Alphabet] [Alphabet]
 
@@ -23,7 +23,8 @@ commandMappings = Map.fromList
                   , ([Alphabet 'j'], Command "moveDown")
                   , ([Alphabet 'k'], Command "moveUp")
                   , ([Alphabet 'l'], Command "moveToRight")
-                  , ([Alphabet 'h', Alphabet '!'], Command "h!")]
+                  , ([Alphabet 'h', Alphabet '!'], Command "h!")
+                  , ([Alphabet 'h', Alphabet 'i', Alphabet '!'], Command "hi!")]
 
 data Result = Accept Command
             | Pending
@@ -35,15 +36,33 @@ translate (x:xs) =
   let
     ps = Map.filterWithKey (\(k:ks) _ -> x == k) commandMappings
     ps' = Map.filterWithKey (\(k:ks) _ -> fromMaybe False $ (==) <$> headMay xs <*> headMay ks) ps
+    xs' = tail xs
+    ps'' = Map.filterWithKey (\(k:ks) _ -> fromMaybe False $ (==) <$> headMay xs' <*> ks `atMay` 1) ps'
+    xs'' = tail xs'
+    ps''' = Map.filterWithKey (\(k:ks) _ -> fromMaybe False $ (==) <$> headMay xs'' <*> ks `atMay` 2) ps''
   in
     case length ps of
       0 -> translate xs
       1 -> (Accept $ snd (Map.findMin ps)) : translate xs
       otherwise -> if null xs then [result (Map.lookup [x] ps)] else 
+        let
+          c2 = Map.lookup [x, head xs] ps'
+        in
                      case length ps' of
                        0 -> maybeToList (Accept <$> Map.lookup [x] ps) ++ translate xs
-                       1 -> (Accept $ snd (Map.findMin ps')) : translate (tail xs)
-                       otherwise -> undefined
+                       1 | c2 /= Nothing ->  result c2 : translate (tail xs)
+                       otherwise -> if null (tail xs) then [result (Map.lookup [x, head xs] ps')] else
+                         let
+                           c3 = Map.lookup [x, head xs, head xs'] ps''
+                         in
+                           case length ps'' of
+                             0 -> maybeToList (Accept <$> Map.lookup [x, head xs] ps') ++ translate xs'
+                             1 | c3 /= Nothing -> result c3 : translate (tail xs')
+                             otherwise -> if null (tail xs') then [result $ Map.lookup [x, head xs, head xs'] ps''] else
+                               case length ps''' of
+                               0 -> maybeToList (Accept <$> Map.lookup [x, head xs, head xs'] ps'') ++ translate xs''
+                               1 -> result (Map.lookup [x, head xs, head xs', head xs''] ps''') : translate (tail xs'')
+                               otherwise -> undefined
 
 result :: Maybe Command -> Result
 result (Just x) = Accept x
