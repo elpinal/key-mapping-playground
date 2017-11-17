272d03e6aa6f1b2a0497e424039ff73121530c8d
elpinal <elpinal@users.noreply.github.com>
2017-11-17 22:27:44 +0900

    Support Operator-Pending mode

--- src/Mapping.hs
+++ src/Mapping.hs
@@ -4,32 +4,44 @@ import Control.Applicative
 
 type Command = Mode -> Maybe Mode
 
-newtype Mode = Mode String
+data Mode =
+    Normal
+  | Visual
+  | Insert
+  | OperatorPending Mode
   deriving Eq
 
-normalMode :: Mode
-normalMode = Mode "normal"
+isOperatorPending :: Mode -> Bool
+isOperatorPending (OperatorPending _) = True
+isOperatorPending _ = False
 
-visualMode :: Mode
-visualMode = Mode "visual"
-
-insertMode :: Mode
-insertMode = Mode "insert"
+afterOperatorPending :: Mode -> Maybe Mode
+afterOperatorPending (OperatorPending m) = return m
+afterOperatorPending _ = Nothing
 
 iCmd :: Command
-iCmd = within normalMode insertMode
+iCmd = within Normal Insert
 
 jCmd :: Command
-jCmd = within normalMode normalMode
+jCmd = within Normal Normal
 
 -- The suffix 'C' means 'capital', so iCmdC means 'capital "i" command' ("I" command).
 iCmdC :: Command
-iCmdC = within normalMode insertMode <||> within visualMode insertMode
+iCmdC = within Normal Insert <||> within Visual Insert
 
 (<||>) = liftA2 (<|>)
+
+iwCmd :: Command
+iwCmd = withPred isOperatorPending afterOperatorPending
 
 within :: Mode -> Mode -> Command
 within m n c =
   if c == m
     then return n
+    else Nothing
+
+withPred :: (Mode -> Bool) -> (Mode -> Maybe Mode) -> Command
+withPred f g c =
+  if f c
+    then g c
     else Nothing
